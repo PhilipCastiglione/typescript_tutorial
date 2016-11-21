91f0ea776e4df8ba39e016c144af6800799cb81c
Philip Castiglione <philipcastiglione@gmail.com>
2016-11-22 10:06:20 +1100

    classes

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "John", lastName: "Doe" };
+var user = new Student("John", "X.", "Doe");
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,12 +1,19 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
 }
 
-function greeter(person: Person) {
+function greeter(person : Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-var user = { firstName: "John", lastName: "Doe" };
+var user = new Student("John", "X.", "Doe");
 
-document.body.innerHTML = greeter(user); 
+document.body.innerHTML = greeter(user);
