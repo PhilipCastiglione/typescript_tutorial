4076fe36c7073789dc816757134574d9e38738e4
Philip Castiglione <philipcastiglione@gmail.com>
2016-11-21 17:29:12 +1100

    type checking at compilation time

--- greeter.ts
+++ greeter.ts
@@ -2,6 +2,7 @@ function greeter(person: string) {
     return "Hello, " + person;
 }
 
-var user = "Jane User";
+var user = [1,2,3];
+// => greeter.ts(7,35): error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'.
 
 document.body.innerHTML = greeter(user); 
